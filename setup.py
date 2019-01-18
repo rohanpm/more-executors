b445d502ae5899a854b034364200e62db1aae297
Rohan McGovern <rohan@mcgovern.id.au>
2019-01-19 07:33:14 +1000

    Release v1.20.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_long_description():
 
 setup(
     name='more-executors',
-    version='1.19.1',
+    version='1.20.0',
     author='Rohan McGovern',
     author_email='rohan@mcgovern.id.au',
     packages=find_packages(exclude=['tests', 'tests.*']),
